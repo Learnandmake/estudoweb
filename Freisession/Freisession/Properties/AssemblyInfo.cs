a8a948290e4da5cc747fe094a9bf1091a028441d
cloudprog <gabrod7k@gmail.com>
2021-05-25 23:22:31 -0300

    a

--- Freisession/Freisession/Properties/AssemblyInfo.cs
+++ Freisession/Freisession/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("freisessionone")]
+[assembly: AssemblyTitle("Freisession")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("freisessionone")]
+[assembly: AssemblyProduct("Freisession")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7af22fa9-5455-456a-bd06-dcfffa1a6695")]
+[assembly: Guid("5c025774-1221-4a57-b163-559ab305b0f5")]
 
 // Version information for an assembly consists of the following four values:
 //
