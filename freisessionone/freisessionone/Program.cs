f974c3ac54fcda092269a7b5c46e8aff7ff35ada
cloudprog <gabrod7k@gmail.com>
2021-05-25 22:57:05 -0300

    helloworld

--- freisessionone/freisessionone/Program.cs
+++ freisessionone/freisessionone/Program.cs
@@ -10,6 +10,8 @@ namespace freisessionone
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
         }
     }
 }
