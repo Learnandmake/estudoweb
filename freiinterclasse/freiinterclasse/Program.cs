b0379cd5cbb899423f1f78ba5557d02f9954c482
cloudprog <gabrod7k@gmail.com>
2021-05-27 22:07:04 -0300

    1 a 4

--- freiinterclasse/freiinterclasse/Program.cs
+++ freiinterclasse/freiinterclasse/Program.cs
@@ -10,6 +10,65 @@ namespace freiinterclasse
     {
         static void Main(string[] args)
         {
-        }
+
+            //interclasse
+
+            functions.interclasse interclassefunction = new functions.interclasse();
+            //1.exercicio
+            Console.WriteLine("Informe um numero");
+            decimal n = Convert.ToInt32(Console.ReadLine());
+
+            bool par = interclassefunction.verificarpar(n);
+
+            if(par == true)
+            {
+                Console.WriteLine("par");
+            }
+
+            else 
+            {
+                Console.WriteLine("ímpar");
+            }
+
+            //2.excercicio
+            Console.WriteLine("Informe o valor de A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor de B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double r = interclassefunction.calcformula(a, b);
+
+            Console.WriteLine(r); 
+
+            //3.excercicio
+            Console.WriteLine("Informe os gastos");
+            double gastos = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe os lucros");
+            double lucros = Convert.ToDouble(Console.ReadLine());
+
+            string r1 = interclassefunction.lucroprejuizo(gastos, lucros);
+                        
+            Console.WriteLine(r1);
+
+            //4.excercicio
+            Console.WriteLine("Informe uma frase:");
+            string frase = Console.ReadLine();
+
+            bool r2 = interclassefunction.possuivogais(frase);
+
+            if(r2== true)
+            {
+                Console.WriteLine("possui todas as vogais");
+            }
+            
+            else
+            {
+                Console.WriteLine("Não possui todas as vogais");
+
+            }
+            
+            } 
+
     }
 }
---
+++ freiinterclasse/freiinterclasse/functions/interclasse.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace freiinterclasse.functions
+{
+    class interclasse
+    { public bool verificarpar(decimal numero)
+    {
+            numero = Math.Ceiling(numero);
+
+            decimal resto = numero% 2;
+
+            if (resto ==0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+
+    }
+
+    public double calcformula (double a, double b)
+    {
+            double s = a / b;
+            double r = Math.Pow(s, 4);
+
+            return r;
+    }
+    public string lucroprejuizo (double gasto,double lucro)
+    {
+            double saldo = lucro - gasto;
+
+            double absoluto = Math.Abs(saldo);
+
+            string r = "";
+
+            if (saldo>=0)
+            {
+                r = "O lucro foi de " + absoluto + " reais";
+
+            }
+    else
+            {
+                r = "O prejuizo foi de " + absoluto + " reais";
+ }
+            return r; 
+            }
+
+           public bool possuivogais (string frase)
+           {
+            frase = frase.ToLower();
+
+            bool a = frase.Contains("a");
+            bool e = frase.Contains("e");
+            bool i = frase.Contains("i");
+            bool o = frase.Contains("o");
+            bool u = frase.Contains("u");
+
+            if (a == true && e ==true &&i == true && o == true && u == true)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
