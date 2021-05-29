cce109a1ba1ccebf67a3af92e150c7cd4b35d769
cloudprog <gabrod7k@gmail.com>
2021-05-29 00:41:14 -0300

    funcionando

--- freidesafioconsole/freidesafioconsole/Program.cs
+++ freidesafioconsole/freidesafioconsole/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Correios;
 namespace freidesafioconsole
 {
     class Program
@@ -12,11 +12,15 @@ namespace freidesafioconsole
         {//SESSIONSIX-DESAFIO
             string cep = "";
             Console.WriteLine("vou consultar seu endereco pelo cep");
-            Console.WriteLine("digite seu cep");
+            Console.WriteLine("digite seu cep com digito");
             cep = Console.ReadLine();
 
-            Correiosapi correios = new Correiosapi();
-           Console.WriteLine("Seu endereco é:  " +correios.Buscar(cep));
+            Correios.CorreiosApi correios = new CorreiosApi();
+            var endereco = correios.consultaCEP(cep);
+
+
+            Console.WriteLine("Seu endereco é:  "+endereco.cidade+" "+ endereco.end +" "+endereco.bairro +" "+ endereco.complemento);
+            Console.ReadKey();
 
 
                     }
