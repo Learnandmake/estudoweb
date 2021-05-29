da96545d7e35a686e6958110f83e68bfb4c74964
cloudprog <gabrod7k@gmail.com>
2021-05-29 17:42:19 -0300

    122

--- freiintensivoconsole/freiintensivoconsole/Program.cs
+++ freiintensivoconsole/freiintensivoconsole/Program.cs
@@ -10,6 +10,26 @@ namespace freiintensivoconsole
     {
         static void Main(string[] args)
         { //1.exercicio
+            Console.WriteLine("informe a nota 1:");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("informe a nota 2:");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("informe a nota 3:");
+            double n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("informe a nota 4:");
+            double n4 = Convert.ToDouble(Console.ReadLine());
+
+            function.intensivo intensivo = new function.intensivo();
+            double media = intensivo.calcularmedia(n1, n2, n3, n4);
+            string sugestao = intensivo.darsugestao(media);
+
+            Console.WriteLine("Sua media é: " + media);
+            Console.WriteLine(sugestao);
+
+            //2.exercicio
 
 
         }
--- freiintensivoconsole/freiintensivoconsole/function/intensivo.cs
+++ freiintensivoconsole/freiintensivoconsole/function/intensivo.cs
@@ -8,5 +8,74 @@ namespace freiintensivoconsole.function
 {
     class intensivo
     {
+    public double calcularmedia (double n1, double n2,double n3, double n4)
+    {
+            double media = (n1 + n2 + n3 + n4) / 4;
+            return media;
+
+    }
+
+    public string darsugestao(double media)
+    {
+            string sugestao = "";
+            if(media >= 0 && media <= 3)
+            {
+                sugestao = "Verifique os pontos de dificuldade antes de continuar";
+
+            }
+
+            else if (media >3 && media <=5)
+            { sugestao = "trabalhe para entender os conceitos antes de programar"; }
+
+            else if (media > 5 && media <= 8)
+            { sugestao = "treine com uma intendidade alta para ficar fluente"; }
+
+            else  
+            { sugestao = "busque treinos que bugam seu cerebro e desafie sua mente"; }
+
+            return sugestao;
+        }
+
+        public double somar( double n1, double n2)
+        {
+            double r = n1 + n2;
+            return r;
+
+        }
+
+        public double subtrair(double n1, double n2)
+        {
+            double r = n1 + n2;
+            return r;
+
+        }
+
+        public double multiplicar(double n1, double n2)
+        {
+            double r = n1 * n2;
+            return r;
+
+        }
+
+        public double dividir(double n1, double n2)
+        {
+            double r = n1 / n2;
+            return r;
+
+        }
+
+        public double potencia(double n, double potencia)
+        {
+            double r = Math.Pow(n,potencia);
+            return r;
+
+        }
+
+        public double raizquadrada(double n)
+        {
+            double r = Math.Sqrt(n);
+            return r;
+
+        }
     }
 }
