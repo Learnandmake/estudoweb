21d7a014feb6e0aaff03ea0ae2b3c3023162f49d
cloudprog <gabrod7k@gmail.com>
2021-05-29 01:20:35 -0300

    desafio email

--- freidesafioconsole/freidesafioconsole/Program.cs
+++ freidesafioconsole/freidesafioconsole/Program.cs
@@ -9,7 +9,34 @@ namespace freidesafioconsole
     class Program
     {
         static void Main(string[] args)
-        {//SESSIONSIX-DESAFIO CEP
+        {   //DESAFIO-EMAIL
+
+            string remetente = "testinmail21@gmail.com";
+            string senha = "98847685";
+
+            string destinatario = "";
+            string assunto = "";
+            string mensagem="";
+
+
+
+            Console.WriteLine("digite o destinatario");
+            destinatario = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("digite o assunto");
+            assunto = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("digite a mensagem");
+            mensagem = Convert.ToString(Console.ReadLine());
+
+            gmailsender gmail = new gmailsender();
+
+            gmail.ConfigurarCredenciais(remetente, senha);
+
+            gmail.Enviar(destinatario, assunto, mensagem);
+            Console.WriteLine("email enviado");
+
+
+
+            //SESSIONSIX-DESAFIO CEP
             string cep = "";
             Console.WriteLine("vou consultar seu endereco pelo cep");
             Console.WriteLine("digite seu cep com digito");
@@ -21,7 +48,10 @@ namespace freidesafioconsole
 
             Console.WriteLine("Seu endereco é:  "+endereco.cidade+" "+ endereco.end +" "+endereco.bairro +" "+ endereco.complemento);
             Console.ReadKey();
-            //DESAFIO-EMAIL
+
+            
+
+
 
                     }
     }
