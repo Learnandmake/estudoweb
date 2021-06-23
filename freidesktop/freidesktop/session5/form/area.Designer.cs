b7326738e5026b70792aede893055681eb5f5b55
cloudprog <gabrod7k@gmail.com>
2021-06-23 10:09:21 -0300

    a

--- freidesktop/freidesktop/session5/form/area.Designer.cs
+++ freidesktop/freidesktop/session5/form/area.Designer.cs
@@ -36,7 +36,37 @@
             this.filosofiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.geografiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.químicaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gm = new System.Windows.Forms.GroupBox();
+            this.gp = new System.Windows.Forms.GroupBox();
+            this.gf = new System.Windows.Forms.GroupBox();
+            this.gg = new System.Windows.Forms.GroupBox();
+            this.gq = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btna = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbla = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblp = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblai = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblad = new System.Windows.Forms.Label();
+            this.lblqp = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
+            this.gm.SuspendLayout();
+            this.gf.SuspendLayout();
+            this.gg.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -49,7 +79,7 @@
             this.químicaToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(366, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(616, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -58,42 +88,290 @@
             this.matematicaToolStripMenuItem.Name = "matematicaToolStripMenuItem";
             this.matematicaToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
             this.matematicaToolStripMenuItem.Text = "Matematica";
+            this.matematicaToolStripMenuItem.Click += new System.EventHandler(this.matematicaToolStripMenuItem_Click);
             // 
             // portuguêsToolStripMenuItem
             // 
             this.portuguêsToolStripMenuItem.Name = "portuguêsToolStripMenuItem";
             this.portuguêsToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
             this.portuguêsToolStripMenuItem.Text = "Português";
+            this.portuguêsToolStripMenuItem.Click += new System.EventHandler(this.portuguêsToolStripMenuItem_Click);
             // 
             // filosofiaToolStripMenuItem
             // 
             this.filosofiaToolStripMenuItem.Name = "filosofiaToolStripMenuItem";
             this.filosofiaToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
             this.filosofiaToolStripMenuItem.Text = "Filosofia";
+            this.filosofiaToolStripMenuItem.Click += new System.EventHandler(this.filosofiaToolStripMenuItem_Click);
             // 
             // geografiaToolStripMenuItem
             // 
             this.geografiaToolStripMenuItem.Name = "geografiaToolStripMenuItem";
             this.geografiaToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
             this.geografiaToolStripMenuItem.Text = "Geografia";
+            this.geografiaToolStripMenuItem.Click += new System.EventHandler(this.geografiaToolStripMenuItem_Click);
             // 
             // químicaToolStripMenuItem
             // 
             this.químicaToolStripMenuItem.Name = "químicaToolStripMenuItem";
             this.químicaToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
             this.químicaToolStripMenuItem.Text = "Química";
+            this.químicaToolStripMenuItem.Click += new System.EventHandler(this.químicaToolStripMenuItem_Click);
+            // 
+            // gm
+            // 
+            this.gm.Controls.Add(this.gf);
+            this.gm.Controls.Add(this.label4);
+            this.gm.Controls.Add(this.lblp);
+            this.gm.Controls.Add(this.label5);
+            this.gm.Controls.Add(this.lbla);
+            this.gm.Controls.Add(this.label3);
+            this.gm.Controls.Add(this.label2);
+            this.gm.Controls.Add(this.numericUpDown2);
+            this.gm.Controls.Add(this.label1);
+            this.gm.Controls.Add(this.numericUpDown1);
+            this.gm.Controls.Add(this.btna);
+            this.gm.Controls.Add(this.pic);
+            this.gm.Controls.Add(this.gg);
+            this.gm.Location = new System.Drawing.Point(12, 27);
+            this.gm.Name = "gm";
+            this.gm.Size = new System.Drawing.Size(342, 223);
+            this.gm.TabIndex = 1;
+            this.gm.TabStop = false;
+            this.gm.Visible = false;
+            // 
+            // gp
+            // 
+            this.gp.Location = new System.Drawing.Point(12, 256);
+            this.gp.Name = "gp";
+            this.gp.Size = new System.Drawing.Size(342, 223);
+            this.gp.TabIndex = 2;
+            this.gp.TabStop = false;
+            this.gp.Visible = false;
+            // 
+            // gf
+            // 
+            this.gf.Controls.Add(this.lblqp);
+            this.gf.Controls.Add(this.lblad);
+            this.gf.Controls.Add(this.label10);
+            this.gf.Controls.Add(this.label9);
+            this.gf.Controls.Add(this.lblai);
+            this.gf.Controls.Add(this.label7);
+            this.gf.Controls.Add(this.textBox1);
+            this.gf.Controls.Add(this.label6);
+            this.gf.Location = new System.Drawing.Point(0, 0);
+            this.gf.Name = "gf";
+            this.gf.Size = new System.Drawing.Size(342, 223);
+            this.gf.TabIndex = 3;
+            this.gf.TabStop = false;
+            this.gf.Visible = false;
+            // 
+            // gg
+            // 
+            this.gg.Controls.Add(this.gq);
+            this.gg.Location = new System.Drawing.Point(28, 267);
+            this.gg.Name = "gg";
+            this.gg.Size = new System.Drawing.Size(342, 154);
+            this.gg.TabIndex = 4;
+            this.gg.TabStop = false;
+            this.gg.Visible = false;
+            // 
+            // gq
+            // 
+            this.gq.Location = new System.Drawing.Point(41, 182);
+            this.gq.Name = "gq";
+            this.gq.Size = new System.Drawing.Size(342, 223);
+            this.gq.TabIndex = 5;
+            this.gq.TabStop = false;
+            this.gq.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Base:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(47, 29);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(63, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // btna
+            // 
+            this.btna.Location = new System.Drawing.Point(35, 81);
+            this.btna.Name = "btna";
+            this.btna.Size = new System.Drawing.Size(75, 23);
+            this.btna.TabIndex = 5;
+            this.btna.Text = "aplicar";
+            this.btna.UseVisualStyleBackColor = true;
+            // 
+            // pic
+            // 
+            this.pic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic.Location = new System.Drawing.Point(140, 92);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(51, 35);
+            this.pic.TabIndex = 6;
+            this.pic.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Altura:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(47, 55);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(63, 20);
+            this.numericUpDown2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(149, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Área:";
+            // 
+            // lbla
+            // 
+            this.lbla.AutoSize = true;
+            this.lbla.Location = new System.Drawing.Point(190, 31);
+            this.lbla.Name = "lbla";
+            this.lbla.Size = new System.Drawing.Size(13, 13);
+            this.lbla.TabIndex = 10;
+            this.lbla.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(128, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Perímetro";
+            // 
+            // lblp
+            // 
+            this.lblp.AutoSize = true;
+            this.lblp.Location = new System.Drawing.Point(190, 57);
+            this.lblp.Name = "lblp";
+            this.lblp.Size = new System.Drawing.Size(13, 13);
+            this.lblp.TabIndex = 12;
+            this.lblp.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(244, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Escolha as informações  para a área do retângulo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(261, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Digite uma frase para o analisador gramatical verificar:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 34);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 61);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 114);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Qtd. Artigos indefinidos:";
+            // 
+            // lblai
+            // 
+            this.lblai.AutoSize = true;
+            this.lblai.Location = new System.Drawing.Point(128, 114);
+            this.lblai.Name = "lblai";
+            this.lblai.Size = new System.Drawing.Size(13, 13);
+            this.lblai.TabIndex = 3;
+            this.lblai.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(17, 138);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(110, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Qtd. Artigos definidos:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(36, 160);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(91, 13);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "Qtd. Preposições:";
+            // 
+            // lblad
+            // 
+            this.lblad.AutoSize = true;
+            this.lblad.Location = new System.Drawing.Point(128, 138);
+            this.lblad.Name = "lblad";
+            this.lblad.Size = new System.Drawing.Size(13, 13);
+            this.lblad.TabIndex = 6;
+            this.lblad.Text = "0";
+            // 
+            // lblqp
+            // 
+            this.lblqp.AutoSize = true;
+            this.lblqp.Location = new System.Drawing.Point(128, 160);
+            this.lblqp.Name = "lblqp";
+            this.lblqp.Size = new System.Drawing.Size(13, 13);
+            this.lblqp.TabIndex = 7;
+            this.lblqp.Text = "0";
             // 
             // area
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(366, 262);
+            this.ClientSize = new System.Drawing.Size(616, 560);
+            this.Controls.Add(this.gp);
+            this.Controls.Add(this.gm);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "area";
             this.Text = "area";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.gm.ResumeLayout(false);
+            this.gm.PerformLayout();
+            this.gf.ResumeLayout(false);
+            this.gf.PerformLayout();
+            this.gg.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,5 +386,29 @@
         private System.Windows.Forms.ToolStripMenuItem filosofiaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem geografiaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem químicaToolStripMenuItem;
+        private System.Windows.Forms.GroupBox gm;
+        private System.Windows.Forms.GroupBox gp;
+        private System.Windows.Forms.GroupBox gf;
+        private System.Windows.Forms.GroupBox gg;
+        private System.Windows.Forms.GroupBox gq;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbla;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btna;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Label lblqp;
+        private System.Windows.Forms.Label lblad;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblai;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
     }
 }
--- freidesktop/freidesktop/session5/form/area.cs
+++ freidesktop/freidesktop/session5/form/area.cs
@@ -16,5 +16,57 @@ namespace freidesktop.session5.form
         {
             InitializeComponent();
         }
+
+        private void matematicaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gm.Visible = true;
+            gp.Visible = false;
+            gf.Visible = false;
+            gq.Visible = false;
+            gg.Visible = false;
+
+        }
+
+   
+        private void portuguêsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+            gm.Visible = false;
+            gp.Visible = true;
+            gf.Visible = false;
+            gq.Visible = false;
+            gg.Visible = false;
+        }
+
+        private void filosofiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            gm.Visible = false;
+            gp.Visible = false;
+            gf.Visible = true;
+            gq.Visible = false;
+            gg.Visible = false;
+        }
+
+        private void geografiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            gm.Visible = false;
+            gp.Visible = false;
+            gf.Visible = false;
+            gq.Visible = false;
+            gg.Visible = true;
+        }
+
+        private void químicaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            gm.Visible = false;
+            gp.Visible = false;
+            gf.Visible = false;
+            gq.Visible = true;
+            gg.Visible = false;
+        }
     }
 }
