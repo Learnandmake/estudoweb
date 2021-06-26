e6759fb06ec50a3653da2ba4b15584b572ff37ac
cloudprog <gabrod7k@gmail.com>
2021-06-26 11:34:58 -0300

    assento

--- freidesktop/freidesktop/session6/form/comprar.cs
+++ freidesktop/freidesktop/session6/form/comprar.cs
@@ -27,93 +27,182 @@ namespace freidesktop.session6.form
         {
         if(h1.BackColor == Color.LimeGreen)
         {
-
+                h1.BackColor = Color.Firebrick;
         }
+        else
+        { h1.BackColor = Color.LimeGreen; }
         }
 
         private void g1_Click(object sender, EventArgs e)
         {
-
-        }
+            if (g1.BackColor == Color.LimeGreen)
+            {
+                g1.BackColor = Color.Firebrick;
+            }
+            else
+            { g1.BackColor = Color.LimeGreen; }
+        
+    }
 
         private void f1_Click(object sender, EventArgs e)
         {
+            if (f1.BackColor == Color.LimeGreen)
+            {
+                f1.BackColor = Color.Firebrick;
+            }
+            else
+            { f1.BackColor = Color.LimeGreen; }
 
         }
 
         private void e1_Click(object sender, EventArgs e)
         {
-
+            if (e1.BackColor == Color.LimeGreen)
+            {
+                e1.BackColor = Color.Firebrick;
+            }
+            else
+            { e1.BackColor = Color.LimeGreen; }
         }
 
         private void d1_Click(object sender, EventArgs e)
         {
-
+            if (d1.BackColor == Color.LimeGreen)
+            {
+                d1.BackColor = Color.Firebrick;
+            }
+            else
+            { d1.BackColor = Color.LimeGreen; }
         }
 
         private void c1_Click(object sender, EventArgs e)
         {
-
+            if (c1.BackColor == Color.LimeGreen)
+            {
+                c1.BackColor = Color.Firebrick;
+            }
+            else
+            { c1.BackColor = Color.LimeGreen; }
         }
 
         private void b1_Click(object sender, EventArgs e)
         {
-
+            if (b1.BackColor == Color.LimeGreen)
+            {
+                b1.BackColor = Color.Firebrick;
+            }
+            else
+            { b1.BackColor = Color.LimeGreen; }
         }
 
         private void a1_Click(object sender, EventArgs e)
         {
-
+            if (a1.BackColor == Color.LimeGreen)
+            {
+                a1.BackColor = Color.Firebrick;
+            }
+            else
+            { a1.BackColor = Color.LimeGreen; }
         }
 
         private void a2_Click(object sender, EventArgs e)
         {
-
+            if (a2.BackColor == Color.LimeGreen)
+            {
+                a2.BackColor = Color.Firebrick;
+            }
+            else
+            { a2.BackColor = Color.LimeGreen; }
         }
 
         private void b2_Click(object sender, EventArgs e)
         {
-
+            if (b2.BackColor == Color.LimeGreen)
+            {
+                b2.BackColor = Color.Firebrick;
+            }
+            else
+            { b2.BackColor = Color.LimeGreen; }
         }
 
         private void c2_Click(object sender, EventArgs e)
         {
-
+            if (c2.BackColor == Color.LimeGreen)
+            {
+                c2.BackColor = Color.Firebrick;
+            }
+            else
+            { c2.BackColor = Color.LimeGreen; }
         }
 
         private void d2_Click(object sender, EventArgs e)
         {
-
+            if (d2.BackColor == Color.LimeGreen)
+            {
+                d2.BackColor = Color.Firebrick;
+            }
+            else
+            { d2.BackColor = Color.LimeGreen; }
         }
 
         private void f2_Click(object sender, EventArgs e)
         {
-
+            if (f2.BackColor == Color.LimeGreen)
+            {
+                f2.BackColor = Color.Firebrick;
+            }
+            else
+            { f2.BackColor = Color.LimeGreen; }
         }
 
         private void g2_Click(object sender, EventArgs e)
         {
-
+            if (g2.BackColor == Color.LimeGreen)
+            {
+                g2.BackColor = Color.Firebrick;
+            }
+            else
+            { g2.BackColor = Color.LimeGreen; }
         }
 
         private void a3_Click(object sender, EventArgs e)
         {
-
+            if (a3.BackColor == Color.LimeGreen)
+            {
+                a3.BackColor = Color.Firebrick;
+            }
+            else
+            { a3.BackColor = Color.LimeGreen; }
         }
 
         private void b3_Click(object sender, EventArgs e)
         {
-
+            if (b3.BackColor == Color.LimeGreen)
+            {
+                b3.BackColor = Color.Firebrick;
+            }
+            else
+            { b3.BackColor = Color.LimeGreen; }
         }
 
         private void c3_Click(object sender, EventArgs e)
         {
-
+            if (c3.BackColor == Color.LimeGreen)
+            {
+                c3.BackColor = Color.Firebrick;
+            }
+            else
+            { c3.BackColor = Color.LimeGreen; }
         }
 
         private void d3_Click(object sender, EventArgs e)
         {
-
+            if (d3.BackColor == Color.LimeGreen)
+            {
+                d3.BackColor = Color.Firebrick;
+            }
+            else
+            { d3.BackColor = Color.LimeGreen; }
         }
     }
 }
