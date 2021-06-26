f021360777bfa3efca7fc99d0eb8d87f5b659d5d
cloudprog <gabrod7k@gmail.com>
2021-06-25 22:38:34 -0300

    a

---
+++ freidesktop/freidesktop/exemplo/frmConceito.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace freidesktop.exemplo
+{
+    public partial class frmConceito : Form
+    {
+        public frmConceito()
+        {
+            InitializeComponent();
+        }
+
+        private void frmConceito_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
