1f30a16978283d8fd6d73fee7088a9738b0fcf56
cloudprog <gabrod7k@gmail.com>
2021-07-04 17:35:25 -0300

    a

---
+++ freidesktop/freidesktop/session9/database/connection.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace freidesktop.session9.database
+{
+    class connection
+    {
+        public MySqlConnection Create()
+        {
+            string connectionString = "server=localhost;database=info2019DB;uid=root;password=1234";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
