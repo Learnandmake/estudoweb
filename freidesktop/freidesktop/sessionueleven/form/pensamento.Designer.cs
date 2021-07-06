d24ccb1be428aa919530c9eb272a75e1ba806933
cloudprog <gabrod7k@gmail.com>
2021-07-06 15:35:22 -0300

    a

--- freidesktop/freidesktop/sessionueleven/form/pensamento.Designer.cs
+++ freidesktop/freidesktop/sessionueleven/form/pensamento.Designer.cs
@@ -28,20 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pensamento));
+            this.txtbnome = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dvg = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mcscontato = new System.Windows.Forms.MaskedTextBox();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.picclose = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dvg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picclose)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtbnome
+            // 
+            this.txtbnome.Location = new System.Drawing.Point(209, 39);
+            this.txtbnome.Name = "txtbnome";
+            this.txtbnome.Size = new System.Drawing.Size(189, 20);
+            this.txtbnome.TabIndex = 30;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.button2.Location = new System.Drawing.Point(403, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 32;
+            this.button2.Text = "ok";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dvg
+            // 
+            this.dvg.AllowUserToAddRows = false;
+            this.dvg.AllowUserToDeleteRows = false;
+            this.dvg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvg.Location = new System.Drawing.Point(173, 65);
+            this.dvg.Name = "dvg";
+            this.dvg.ReadOnly = true;
+            this.dvg.Size = new System.Drawing.Size(343, 185);
+            this.dvg.TabIndex = 31;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(175, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Nome:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.SteelBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(631, 27);
+            this.pictureBox1.TabIndex = 35;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.mcscontato);
+            this.groupBox1.Controls.Add(this.txtnome);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(11, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(147, 108);
+            this.groupBox1.TabIndex = 34;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Cel:";
+            // 
+            // mcscontato
+            // 
+            this.mcscontato.Location = new System.Drawing.Point(46, 43);
+            this.mcscontato.Mask = "(00)00000-0000";
+            this.mcscontato.Name = "mcscontato";
+            this.mcscontato.Size = new System.Drawing.Size(88, 20);
+            this.mcscontato.TabIndex = 13;
+            // 
+            // txtnome
+            // 
+            this.txtnome.Location = new System.Drawing.Point(46, 17);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(88, 20);
+            this.txtnome.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.button1.Location = new System.Drawing.Point(46, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "ok";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nome:";
+            // 
+            // picclose
+            // 
+            this.picclose.BackColor = System.Drawing.Color.SteelBlue;
+            this.picclose.Image = ((System.Drawing.Image)(resources.GetObject("picclose.Image")));
+            this.picclose.Location = new System.Drawing.Point(572, -1);
+            this.picclose.Name = "picclose";
+            this.picclose.Size = new System.Drawing.Size(34, 27);
+            this.picclose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picclose.TabIndex = 36;
+            this.picclose.TabStop = false;
             // 
             // pensamento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(608, 262);
+            this.Controls.Add(this.picclose);
+            this.Controls.Add(this.txtbnome);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dvg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "pensamento";
             this.Text = "pensamento";
+            ((System.ComponentModel.ISupportInitialize)(this.dvg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picclose)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtbnome;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dvg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox mcscontato;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox picclose;
     }
 }
