6c21cbac4e70096da081c32d308e098875e7cdba
cloudprog <gabrod7k@gmail.com>
2021-07-07 20:24:42 -0300

    sasa

---
+++ freientity/freientity/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace freientity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dvg = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dvg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dvg
+            // 
+            this.dvg.AllowUserToAddRows = false;
+            this.dvg.AllowUserToDeleteRows = false;
+            this.dvg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvg.Location = new System.Drawing.Point(12, 12);
+            this.dvg.Name = "dvg";
+            this.dvg.ReadOnly = true;
+            this.dvg.Size = new System.Drawing.Size(337, 150);
+            this.dvg.TabIndex = 0;
+            this.dvg.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvg_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 183);
+            this.Controls.Add(this.dvg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dvg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dvg;
+    }
+}
+
---
+++ freientity/freientity/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace freientity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            database.entity.schooldbEntities a = new database.entity.schooldbEntities();
+            dvg.DataSource = a.tbturmas.ToList();
+        }
+
+        private void dvg_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
