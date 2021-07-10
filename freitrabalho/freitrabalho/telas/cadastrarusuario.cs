d073335092818abede6670706df2b57ab67c8281
cloudprog <gabrod7k@gmail.com>
2021-07-10 19:19:10 -0300

    asasassasasas

--- freitrabalho/freitrabalho/telas/cadastrarusuario.cs
+++ freitrabalho/freitrabalho/telas/cadastrarusuario.cs
@@ -20,37 +20,46 @@ namespace freitrabalho.telas
 
         private void btncadastrar_Click(object sender, EventArgs e)
         {
-            string nome = Convert.ToString(txtnome.Text);
-            string nick = Convert.ToString(txtnickusuario.Text);
-            string email = Convert.ToString(txtnickusuario.Text);
-            string senha = Convert.ToString(txtnickusuario.Text);
+        try 
+        {
+                string nome = Convert.ToString(txtnome.Text);
+                string nick = Convert.ToString(txtnickusuario.Text);
+                string email = Convert.ToString(txtnickusuario.Text);
+                string senha = Convert.ToString(txtnickusuario.Text);
 
-            bussines.bussineslogin blogin = new bussines.bussineslogin();
-            bussines.bussinespergunta bpergunta = new bussines.bussinespergunta();
-            bussines.bussinespontuacao bpontuacao = new bussines.bussinespontuacao();
-            bussines.bussinesusuario busuario = new bussines.bussinesusuario();
-            function.criptografia criptografia = new function.criptografia();
-            string senha2 = criptografia.gerarmd5(senha);
+                bussines.bussineslogin blogin = new bussines.bussineslogin();
+                bussines.bussinespergunta bpergunta = new bussines.bussinespergunta();
+                bussines.bussinespontuacao bpontuacao = new bussines.bussinespontuacao();
+                bussines.bussinesusuario busuario = new bussines.bussinesusuario();
+                function.criptografia criptografia = new function.criptografia();
+                function.vemail fvemail = new function.vemail();
+                string senha2 = criptografia.gerarmd5(senha);
+                bool vemail = fvemail.isemail(email);
 
-            database.entity.tblogin login = new database.entity.tblogin();
-            database.entity.tbpergunta pergunta = new database.entity.tbpergunta();
-            database.entity.tbpontuacao pontuacao = new database.entity.tbpontuacao();
-            database.entity.tbusuario usuario = new database.entity.tbusuario();
+                database.entity.tblogin login = new database.entity.tblogin();
+                database.entity.tbpergunta pergunta = new database.entity.tbpergunta();
+                database.entity.tbpontuacao pontuacao = new database.entity.tbpontuacao();
+                database.entity.tbusuario usuario = new database.entity.tbusuario();
 
-            usuario.nick = nick;
-            usuario.email = email;
-            usuario.registro = DateTime.Now;
-            login.nick = nick;
-            login.senha = senha2;
-            pontuacao.nick = nick;
-            pontuacao.pontuacao = 0;
+                if (vemail == true)
+                {
+                    usuario.nick = nick;
+                    usuario.email = email;
+                    usuario.registro = DateTime.Now;
+                    login.nick = nick;
+                    login.senha = senha2;
+                    pontuacao.nick = nick;
+                    pontuacao.pontuacao = 0;
 
-            busuario.inserir(usuario);
-            blogin.inserir(login);
-            bpontuacao.inserir(pontuacao);
-            MessageBox.Show("Operação bem sucedida");
+                    busuario.inserir(usuario);
+                    blogin.inserir(login);
+                    bpontuacao.inserir(pontuacao);
+                    MessageBox.Show("Operação bem sucedida");
 
-
+                }
+            }
+           catch(Exception)
+            { MessageBox.Show("erro verifique o formulario"); }
         }
     }
 }
