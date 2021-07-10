9248247e2b78f28ee926f111456a29407a549bf6
cloudprog <gabrod7k@gmail.com>
2021-07-10 15:31:56 -0300

    opa

--- freichallenge/freichallenge/Form1.cs
+++ freichallenge/freichallenge/Form1.cs
@@ -32,25 +32,9 @@ namespace freichallenge
             bussines.bussinesaluno baluno = new bussines.bussinesaluno();
             data.entity.tbaluno aluno = dvg1.CurrentRow.DataBoundItem as data.entity.tbaluno;
             data.entity.tbaluno aluno2 = new data.entity.tbaluno();
-            var ultimoaluno = banco.ultimaturma(idturma.idturma);
-            int total = ultimoaluno.idaluno ;
-            int numero = 1; 
-  
-            var posicao1 = banco.idprimeiro(idturma.idturma);
-             int posicao =posicao1.idaluno;
-            int asas = 0;
-            while (posicao <=total )
-            {
-                aluno2 =  banco.filtarporid(posicao);
-                aluno2.nrchamada = numero;        
-                banco.alterar(aluno2);
-               
-                posicao++;
-              while(posicao >=2)
-             {
-                    numero++;
-             }
-            }
+
+            banco.alterar(aluno);
+          
         }
 
         private void cbo1_SelectedIndexChanged(object sender, EventArgs e)
--- freichallenge/freichallenge/data/databasealuno.cs
+++ freichallenge/freichallenge/data/databasealuno.cs
@@ -10,45 +10,63 @@ namespace freichallenge.data
     {
         entity.schooldbEntities banco = new entity.schooldbEntities();
 
-        public entity.tbaluno ultimaturma(int idturma)
-        {
-            entity.tbaluno a = banco.tbaluno.Last(t => t.idturma == idturma);
-            return a;
-        }
+     
         public List<data.entity.tbaluno> filtrarporturma(int idturma)
         {
-            List<data.entity.tbaluno> lista = banco.tbaluno.Where(t => t.idturma == idturma).ToList();
+            List<data.entity.tbaluno> lista = banco. tbaluno.Where(t => t.idturma == idturma).ToList();
             return lista;
         }
         public List<data.entity.tbaluno> ltodos()
         {
-            List<data.entity.tbaluno> lista = banco.tbaluno.ToList();
+            List<data.entity.tbaluno> lista = banco.tbaluno.OrderBy(t=>t.nmaluno).ToList();
             return lista;
         }
-        public entity.tbaluno idprimeiro(int idturma)
-        {
-            entity.tbaluno a = banco.tbaluno.First(t => t.idturma == idturma);
-            return a;
-        }
-         
-        public entity.tbaluno filtarporid(int id )
-        { 
-            entity.tbaluno lista = banco.tbaluno.FirstOrDefault(t => t.idturma == id);
-            return lista;
-        }
-
+        public entity.tbaluno filtrarporid (int idaluno, int idturma)
+         {
+            entity.tbaluno aluno = banco.tbaluno.OrderBy(t=>t.nmaluno).FirstOrDefault(t => t.idaluno == idaluno &&  t.idturma == idturma);
+        
+               while (aluno == null)
+           {
+                idaluno++;
+               aluno = banco.tbaluno.FirstOrDefault(t => t.idaluno == idaluno && t.idturma == idturma);
+            }
+            return aluno;
+         }
+  
         public void alterar(data.entity.tbaluno aluno)
-        {    
-            entity.tbaluno alterar = banco.tbaluno. FirstOrDefault(t => t.idturma == aluno.idturma);
-           if (alterar !=null)
-           {      
-                alterar.nmaluno = aluno.nmaluno;
-                alterar.nrchamada = aluno.nrchamada;
-                alterar.dtnascimento = aluno.dtnascimento;
-                alterar.dsbairro = aluno.dsbairro;
-                alterar.dsmunicipio = aluno.dsmunicipio;
-           }
-            banco.SaveChanges();
+        {
+            // loop para busca
+           int posicao = 1;
+           int numero = 1;
+           int total = banco.tbaluno.Count(t => t.idturma == aluno.idturma);
+           entity.tbaluno listaraluno = banco.tbaluno.FirstOrDefault(t => t.idaluno == posicao && t.idturma == aluno.idturma);
+
+            while (aluno == null)
+            {
+               posicao++;
+                listaraluno = banco.tbaluno.FirstOrDefault(t => t.idaluno == posicao && t.idturma ==aluno.idturma);
+            }
+          //erro
+            while (posicao <= total && aluno != null)
+            {
+                aluno.nrchamada = numero;
+                entity.tbaluno alterar = banco.tbaluno.FirstOrDefault(t =>t.idaluno ==aluno.idaluno);
+                if (alterar != null)
+                {
+                    alterar.nmaluno =listaraluno.nmaluno;
+                    alterar.nrchamada = listaraluno.nrchamada;
+                 
+                  
+                }
+                banco.SaveChanges();
+                posicao++;
+                while (posicao>= 2)
+             {
+                    numero++;
+             }
+                
+            }
+         
           
         }
     }
