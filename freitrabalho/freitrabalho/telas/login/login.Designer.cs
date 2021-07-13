90f64510b0c8448700a05e34e35912a926895b52
cloudprog <gabrod7k@gmail.com>
2021-07-13 11:17:32 -0300

    a

--- freitrabalho/freitrabalho/telas/login/login.Designer.cs
+++ freitrabalho/freitrabalho/telas/login/login.Designer.cs
@@ -29,7 +29,154 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            this.picclose = new System.Windows.Forms.PictureBox();
+            this.picbarra = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtsenha = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtnickusuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btncadastrar = new System.Windows.Forms.Button();
+            this.lblcadastro = new System.Windows.Forms.Label();
+            this.lblrecusenha = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picclose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbarra)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // picclose
+            // 
+            this.picclose.BackColor = System.Drawing.Color.Teal;
+            this.picclose.Image = ((System.Drawing.Image)(resources.GetObject("picclose.Image")));
+            this.picclose.Location = new System.Drawing.Point(253, -1);
+            this.picclose.Name = "picclose";
+            this.picclose.Size = new System.Drawing.Size(39, 31);
+            this.picclose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picclose.TabIndex = 16;
+            this.picclose.TabStop = false;
+            // 
+            // picbarra
+            // 
+            this.picbarra.BackColor = System.Drawing.Color.Teal;
+            this.picbarra.Location = new System.Drawing.Point(-10, -1);
+            this.picbarra.Name = "picbarra";
+            this.picbarra.Size = new System.Drawing.Size(302, 31);
+            this.picbarra.TabIndex = 14;
+            this.picbarra.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 73);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(85, 32);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(109, 101);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.txtsenha);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtnickusuario);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(46, 128);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(190, 77);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtsenha
+            // 
+            this.txtsenha.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsenha.ForeColor = System.Drawing.Color.Black;
+            this.txtsenha.Location = new System.Drawing.Point(73, 48);
+            this.txtsenha.Name = "txtsenha";
+            this.txtsenha.Size = new System.Drawing.Size(100, 23);
+            this.txtsenha.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(1, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 19);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Senha:";
+            // 
+            // txtnickusuario
+            // 
+            this.txtnickusuario.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnickusuario.ForeColor = System.Drawing.Color.Black;
+            this.txtnickusuario.Location = new System.Drawing.Point(73, 19);
+            this.txtnickusuario.Name = "txtnickusuario";
+            this.txtnickusuario.Size = new System.Drawing.Size(100, 23);
+            this.txtnickusuario.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(15, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 19);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nick:";
+            // 
+            // btncadastrar
+            // 
+            this.btncadastrar.BackColor = System.Drawing.Color.MintCream;
+            this.btncadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncadastrar.ForeColor = System.Drawing.Color.Teal;
+            this.btncadastrar.Location = new System.Drawing.Point(46, 211);
+            this.btncadastrar.Name = "btncadastrar";
+            this.btncadastrar.Size = new System.Drawing.Size(190, 44);
+            this.btncadastrar.TabIndex = 17;
+            this.btncadastrar.Text = "Login";
+            this.btncadastrar.UseVisualStyleBackColor = false;
+            // 
+            // lblcadastro
+            // 
+            this.lblcadastro.AutoSize = true;
+            this.lblcadastro.BackColor = System.Drawing.Color.Transparent;
+            this.lblcadastro.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcadastro.ForeColor = System.Drawing.Color.White;
+            this.lblcadastro.Location = new System.Drawing.Point(84, 258);
+            this.lblcadastro.Name = "lblcadastro";
+            this.lblcadastro.Size = new System.Drawing.Size(116, 19);
+            this.lblcadastro.TabIndex = 18;
+            this.lblcadastro.Text = "Cadastre-se";
+            // 
+            // lblrecusenha
+            // 
+            this.lblrecusenha.AutoSize = true;
+            this.lblrecusenha.BackColor = System.Drawing.Color.Transparent;
+            this.lblrecusenha.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblrecusenha.ForeColor = System.Drawing.Color.White;
+            this.lblrecusenha.Location = new System.Drawing.Point(67, 291);
+            this.lblrecusenha.Name = "lblrecusenha";
+            this.lblrecusenha.Size = new System.Drawing.Size(156, 19);
+            this.lblrecusenha.TabIndex = 19;
+            this.lblrecusenha.Text = "Recuperar senha";
             // 
             // login
             // 
@@ -37,14 +184,41 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(284, 334);
+            this.Controls.Add(this.lblrecusenha);
+            this.Controls.Add(this.lblcadastro);
+            this.Controls.Add(this.picclose);
+            this.Controls.Add(this.picbarra);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btncadastrar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "login";
             this.Text = "login";
+            ((System.ComponentModel.ISupportInitialize)(this.picclose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbarra)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picclose;
+        private System.Windows.Forms.PictureBox picbarra;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtsenha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtnickusuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btncadastrar;
+        private System.Windows.Forms.Label lblcadastro;
+        private System.Windows.Forms.Label lblrecusenha;
     }
 }
