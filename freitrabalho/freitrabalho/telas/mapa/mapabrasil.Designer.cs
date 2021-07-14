d8769e0aa6da9301c4526f9f20554fa568989eb0
cloudprog <gabrod7k@gmail.com>
2021-07-14 16:48:08 -0300

    inicio de mapa

--- freitrabalho/freitrabalho/telas/mapa/mapabrasil.Designer.cs
+++ freitrabalho/freitrabalho/telas/mapa/mapabrasil.Designer.cs
@@ -29,20 +29,157 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mapabrasil));
+            this.picclose = new System.Windows.Forms.PictureBox();
+            this.picbarra = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.lblevento2 = new System.Windows.Forms.Label();
+            this.lblevento1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblsinopse = new System.Windows.Forms.Label();
+            this.lbltitulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picclose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbarra)).BeginInit();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // picclose
+            // 
+            this.picclose.BackColor = System.Drawing.Color.Teal;
+            this.picclose.Image = ((System.Drawing.Image)(resources.GetObject("picclose.Image")));
+            this.picclose.Location = new System.Drawing.Point(677, 0);
+            this.picclose.Name = "picclose";
+            this.picclose.Size = new System.Drawing.Size(39, 31);
+            this.picclose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picclose.TabIndex = 30;
+            this.picclose.TabStop = false;
+            this.picclose.Click += new System.EventHandler(this.picclose_Click);
+            this.picclose.MouseEnter += new System.EventHandler(this.picclose_MouseEnter);
+            this.picclose.MouseLeave += new System.EventHandler(this.picclose_MouseLeave);
+            // 
+            // picbarra
+            // 
+            this.picbarra.BackColor = System.Drawing.Color.Teal;
+            this.picbarra.Location = new System.Drawing.Point(-8, 0);
+            this.picbarra.Name = "picbarra";
+            this.picbarra.Size = new System.Drawing.Size(737, 31);
+            this.picbarra.TabIndex = 28;
+            this.picbarra.TabStop = false;
+            this.picbarra.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picbarra_MouseMove);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.label3.Font = new System.Drawing.Font("Ravie", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(322, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 26);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "?";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.Color.Teal;
+            this.groupBox4.Controls.Add(this.lblevento2);
+            this.groupBox4.Controls.Add(this.lblevento1);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.lblsinopse);
+            this.groupBox4.Controls.Add(this.lbltitulo);
+            this.groupBox4.Location = new System.Drawing.Point(487, 24);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(229, 403);
+            this.groupBox4.TabIndex = 34;
+            this.groupBox4.TabStop = false;
+            // 
+            // lblevento2
+            // 
+            this.lblevento2.AutoSize = true;
+            this.lblevento2.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblevento2.ForeColor = System.Drawing.Color.White;
+            this.lblevento2.Location = new System.Drawing.Point(17, 271);
+            this.lblevento2.Name = "lblevento2";
+            this.lblevento2.Size = new System.Drawing.Size(78, 19);
+            this.lblevento2.TabIndex = 22;
+            this.lblevento2.Text = "Eventos";
+            // 
+            // lblevento1
+            // 
+            this.lblevento1.AutoSize = true;
+            this.lblevento1.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblevento1.ForeColor = System.Drawing.Color.White;
+            this.lblevento1.Location = new System.Drawing.Point(17, 236);
+            this.lblevento1.Name = "lblevento1";
+            this.lblevento1.Size = new System.Drawing.Size(78, 19);
+            this.lblevento1.TabIndex = 21;
+            this.lblevento1.Text = "Eventos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(17, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 19);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Eventos";
+            // 
+            // lblsinopse
+            // 
+            this.lblsinopse.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsinopse.ForeColor = System.Drawing.Color.White;
+            this.lblsinopse.Location = new System.Drawing.Point(17, 57);
+            this.lblsinopse.Name = "lblsinopse";
+            this.lblsinopse.Size = new System.Drawing.Size(188, 137);
+            this.lblsinopse.TabIndex = 19;
+            this.lblsinopse.Text = "sinopse";
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.ForeColor = System.Drawing.Color.White;
+            this.lbltitulo.Location = new System.Drawing.Point(17, 14);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(181, 19);
+            this.lbltitulo.TabIndex = 18;
+            this.lbltitulo.Text = "titulo";
             // 
             // mapabrasil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(716, 427);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.picclose);
+            this.Controls.Add(this.picbarra);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "mapabrasil";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mapabrasil_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.picclose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picbarra)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picclose;
+        private System.Windows.Forms.PictureBox picbarra;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label lblevento2;
+        private System.Windows.Forms.Label lblevento1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblsinopse;
+        private System.Windows.Forms.Label lbltitulo;
     }
 }
--- freitrabalho/freitrabalho/telas/mapa/mapabrasil.cs
+++ freitrabalho/freitrabalho/telas/mapa/mapabrasil.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,45 @@ namespace freitrabalho.telas.mapa
         public mapabrasil()
         {
             InitializeComponent();
+        }
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        private void picclose_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+
+        private void picclose_MouseEnter(object sender, EventArgs e)
+        {
+            picclose.BackColor = Color.Maroon;
+        }
+
+        private void picclose_MouseLeave(object sender, EventArgs e)
+        {
+            picclose.BackColor = Color.Teal;
+        }
+
+        private void picbarra_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void mapabrasil_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
