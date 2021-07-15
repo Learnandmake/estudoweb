000a9f0e56311ad058960b341e6619e083b73349
cloudprog <gabrod7k@gmail.com>
2021-07-15 14:29:30 -0300

    model

--- freioficina/freioficina/model/livromodel.cs
+++ freioficina/freioficina/model/livromodel.cs
@@ -6,6 +6,11 @@ using System.Web;
 namespace freioficina.model
 {
     public class livromodel
-    {
+    {public int idlivro { get; set; }
+    public string nmlivro { get; set; }
+    public string nmautor { get; set; }
+    public DateTime dtlancamento { get; set; }
+    public DateTime leitura { get; set; }
+    public bool lido { get; set; }
     }
 }
