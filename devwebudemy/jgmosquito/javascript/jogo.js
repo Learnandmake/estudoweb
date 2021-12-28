84cb20e6c0e8479e0fb48bc714d28b42bb50806a
learnandmake <gabrod7k@gmail.com>
2021-12-27 21:18:08 -0300

    jgmosquito-pt3

--- devwebudemy/jgmosquito/javascript/jogo.js
+++ devwebudemy/jgmosquito/javascript/jogo.js
@@ -12,8 +12,8 @@ ajustartamanhopalcojogo();
 
 function posicaorandomica()
 {
-    var posicaox= Math.floor(Math.random() * largura) -180
-    var posicaoy=Math.floor(Math.random() * altura) -180
+    var posicaox= Math.floor(Math.random() * largura) -90
+    var posicaoy=Math.floor(Math.random() * altura) -90
     
     posicaox=posicaox < 0 ? 0 : posicaox
     posicaoy=posicaoy <0 ? 0 :posicaoy
@@ -22,12 +22,31 @@ function posicaorandomica()
     
     var mosquito = document.createElement('img')
     mosquito.src = 'https://lh3.googleusercontent.com/pw/AM-JKLUvO_YoNMhoW6R-9_RvSEyS9tSWwolPybHPdvU1vAQVvbaKNHLY384NqWjOfGo0Yu8YCrWfxKSyjPBgJW7VJGvk0D4-mRqNJ09ASO8uo1rlnExKuz1Hqn7JyOs84jHiLmi8hbPw6b8q1-e6Pa5OIA0R=w606-h625-no?authuser=4'
-    mosquito.className= 'mosquito1'
+    mosquito.className= tamanhoaleatorico()
     mosquito.style.left=posicaox +'px'
     mosquito.style.top=posicaoy +'px'
     mosquito.style.position = 'absolute'
 
 
     document.body.appendChild(mosquito)
+
+    console.log(tamanhoaleatorico())
 }
 
+function tamanhoaleatorico()
+{
+    var classe = Math.floor(Math.random()* 3)
+switch(classe)
+{
+    case 0:
+    
+    return 'mosquito1'
+
+    case 1:
+    
+        return 'mosquito2'
+    case 2:
+            
+    return 'mosquito3'
+}
+}
