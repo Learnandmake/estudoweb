6caab72cd4f80ec2218be5fd050eb89ec2451cb7
Gabriel Rodrigues Santos <sangames014@outlook.com>
2022-08-17 21:25:06 -0300

    170820222

--- devwebudemy/apporcamento/appjs/app.js
+++ devwebudemy/apporcamento/appjs/app.js
@@ -1,3 +1,19 @@
+//objeto despesa
+class despesa
+{
+    //nao esquecer de definir parametros para o constuctor
+    constructor(ano,mes,dia,tipo,descricao,valor)
+    {
+       this.ano = ano
+       this.mes = mes
+       this.dia = dia
+       this.tipo = tipo
+       this.descricao = descricao
+       this.valor = valor
+    }
+}
+
+//cadastra despesas
 function cadastrardespesa(){
 //variaveis
  let ano = document.getElementById('ano')
@@ -6,6 +22,9 @@ function cadastrardespesa(){
  let tipo = document.getElementById('tipo')
  let descricao = document.getElementById('descricao')
  let valor = document.getElementById('valor')
- 
-console.log(ano.value, mes.value, dia.value,tipo.value, descricao.value, valor.value)
+
+// instancia o objeto despesa 
+let depesains = new despesa (ano.value, mes.value, dia.value,tipo.value, descricao.value, valor.value)
+
+console.log(depesains)
 }
