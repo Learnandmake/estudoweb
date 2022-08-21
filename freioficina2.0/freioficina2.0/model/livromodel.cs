328d157e29b607e31bda21b02cb5ab1158ef381d
Gabriel Rodrigues Santos <sangames014@outlook.com>
2022-08-20 22:14:57 -0300

    a

---
+++ freioficina2.0/freioficina2.0/model/livromodel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace freioficina.model
+{
+    public class livromodel
+    {
+        [Display(Name="ID:")]
+        public int idlivro { get; set; }
+        [Display(Name = "Titulo:")]
+        public string nmlivro { get; set; }
+        [Display(Name = "Nome do autor:")]
+        public string nmautor { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de lançamento:")]    
+        public DateTime dtlancamento { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de leitura:")]
+        public DateTime dtleitura { get; set; }
+        [Display(Name = "Já lido:")]
+        public bool lido { get; set; }
+    }
+}
