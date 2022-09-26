53d2a061e60ef911e4ae27c424c4940e02efb7f1
Gabriel Rodrigues Santos <sangames014@outlook.com>
2022-09-26 17:05:44 -0300

    receba

---
+++ devwebudemy/php/aula.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+    <meta charset="utf-8"/>
+    <title> cursophp</title>
+    </head>
+    <body>
+    <?php
+    echo ('aula1');
+    ?>
+    <br/>
+    <?php
+    echo ('utilizando a tag padrão');
+    ?>
+    <br/>
+    <?php
+     echo ('os navegadores não interpretam php');
+    ?>
+      <br/>
+    <?php
+      echo ('html precisa vir pronto do back end');
+    ?>
+    <br/>
+    <?= ' utilizando tag de impressão' ?>
+    <br/>
+    <?php
+    echo ('Aula2');
+    ?>
+    <br/>
+     <?php
+    echo ('tag curta');
+    ?>
+    <br/>
+    </body>
+</html>
