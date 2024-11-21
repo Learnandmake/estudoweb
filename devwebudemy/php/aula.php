26cf195a627e3aa79ae058a728184488ad8cb45d
Gabriel Rodrigues Santos <sangames014@outlook.com>
2024-11-21 14:03:03 -0300

    erros

--- devwebudemy/php/aula.php
+++ devwebudemy/php/aula.php
@@ -1,56 +1,25 @@
 <html>
     <head>
     <meta charset="utf-8"/>
-    <title> cursophp</title>
+    <title> cursophp2
+        
+    </title>
     </head>
     <body>
     
     <?php
-    //a 308
-    //o php tem um vasto suporte a protocolos da internet com programação orientada
-    // a objeto 
-    //suporte a diversos banco de dados
-    // suporta protocolos de comunicação
-
-    //a309
-
-    //embutindo bloos php em paginas html
-
-    //nome
-    //tag padrao      <?php codigo aqui habilitada
-    //tag de impressao  <?= codgo aqui  habilitada
-    //tag curta          <?             desabilitada
-    //asp tag             <%            descontinuada na versao 7
+   echo 'utilizando a tag padrão'; 
+    ?>
+    <br/>
+    <?= 'utilizando a tag de impressão'; ?>
+    <br/>
+    <? echo 'utilizando tag curta'; ?>
     
-    // as codificações php não sao entregues para o browser 
-    // sendo assim as tags php não aparecem no console
-   
-    echo ('aula1');
-    ?>
-    <br/>
-    <?php
-    echo ('utilizando a tag padrão');
-    ?>
-    <br/>
-    <?php
-     echo ('os navegadores não interpretam php');
-    ?>
-      <br/>
-    <?php
-      echo ('html precisa vir pronto do back end'); 
-    ?>
-    <br/>
-    <?= ' utilizando tag de impressão' ?>
-    <br/>
-    <?php
-    echo ('Aula2');
-    ?>
-    <br/>
-     <?php
-    echo ('tag curta');
-    ?>
-    <br/>
+    <?php>
 
+    echo 'comando echo';
+    print "comando print";
+?>
 
     </body>
 </html>
