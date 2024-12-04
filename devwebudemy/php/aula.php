dbebf7fbd47584b31b83816be96e724b366a45b2
Gabriel Rodrigues Santos <sangames014@outlook.com>
2024-12-03 23:14:38 -0300

    031224

--- devwebudemy/php/aula.php
+++ devwebudemy/php/aula.php
@@ -8,18 +8,56 @@
     <body>
     
     <?php
+    //area de teste
+    //a308 ate a310
    echo 'utilizando a tag padrão'; 
     ?>
     <br/>
     <?= 'utilizando a tag de impressão'; ?>
     <br/>
     <? echo 'utilizando tag curta'; ?>
-    
-    <?php>
-
+    <br/>
+    <?php
+//a311 saida de dados com echo e print
     echo 'comando echo';
-    print "comando print";
+   
+?>
+<br/>
+<?php
+ print "comando print";
+
 ?>
 
+<?php
+//a312 funcionamento do php diferenças entre paginas estaticas e dinamicas
+
+// a principal diferença entre as duas é que a pagina estatica nao usa servidor portanto nao
+// e tratada com um servidor mas sim de uma forma simples
+// ja a pagina dinamica tem o conceito mais complexo porque utiliza servidores para tratamento de dados
+// que usamos no site com isso tudo a pagina dinamica tem mais etapadas de processamento do que o pagina estatica
+
+//foto para esclarecer a aula 
+//https://photos.fife.usercontent.google.com/pw/AP1GczPaqIULUh2f3Gz9gszNpGZpbzZdD1Jvi7meg2RPam_UwFZA1W8HU1W4=w1024-h572-s-no-gm?authuser=0
+
+//a313 comentários
+
+# comentário shell 
+//comentario de 1 linha
+/* 
+comentarios de varias linhas 
+*/
+?>
+
+<!--comentário em html (fica exposto para o usuario-->
+<?php 
+// a314 variaveis 
+
+//variaveis em php devem iniciar com a tag $ e não podem conter espaços nem caracters especiais 
+// as variaveis em php nao devem ser especificadas no momento da declaração apenas no momento da inserção de dados
+
+//php diferencia letras maiusculas  e minusculas Nome e Variavel e variavel pro php são 2 variaveis diferentes
+
+
+?>
     </body>
 </html>
